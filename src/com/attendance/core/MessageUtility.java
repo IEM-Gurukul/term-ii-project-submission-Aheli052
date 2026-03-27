65e7a98168d64b91ccb9054458bee50632805559
Aheli052 <ahelibhaumikkgp@gmail.com>
2026-03-27 21:41:18 +0530

    8)Adding a Utility File for simple formatting
    I created a new file called MessageUtility. It has one simple job: to print a straight line in the console. This helps separate different parts of the output so the results are much easier for me to read during testing.

---
+++ src/com/attendance/core/MessageUtility.java
@@ -0,0 +1,8 @@
+package com.attendance.core;
+
+// This file helps us print nice lines in the terminal
+public class MessageUtility {
+    public static void printLine() {
+        System.out.println("------------------------------------");
+    }
+}
