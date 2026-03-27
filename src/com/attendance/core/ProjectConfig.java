446217e1db32ec2573f901eed91a251a9cf19681
Aheli052 <ahelibhaumikkgp@gmail.com>
2026-03-27 21:25:51 +0530

    6)Adding Configuration File for project
    I created this new file to store important details like the college name and academic year. This keeps our main classes clean and makes it easier to manage general settings.

---
+++ src/com/attendance/core/ProjectConfig.java
@@ -0,0 +1,7 @@
+package com.attendance.core;
+
+// This file stores project-wide constants
+public class ProjectConfig {
+    public static final String COLLEGE_NAME = "IEM Kolkata";
+    public static final String ACADEMIC_YEAR = "2025-2026";
+}
