b18202bd73ab851bd2db1ffc12d8dc4ae93582d0
Aheli052 <ahelibhaumikkgp@gmail.com>
2026-03-27 21:48:44 +0530

    10)Adding Project Status & Version information
    I created this final file called ProjectStatus. It shows that the project is now in its final version and ready for submission. I added the date of my last test to show that everything is up to date.

---
+++ src/com/attendance/core/ProjectStatus.java
@@ -0,0 +1,8 @@
+package com.attendance.core;
+
+// This file shows the current state of the project
+public class ProjectStatus {
+    public static final String VERSION = "Version 1.0";
+    public static final String BUILD_TYPE = "Stable - Submission Ready";
+    public static final String LAST_TEST = "27-03-2026";
+}
