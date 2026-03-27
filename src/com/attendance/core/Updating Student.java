6262422ede8a50502e52477052ac50e1c98f941b
Aheli052 <ahelibhaumikkgp@gmail.com>
2026-03-27 21:16:54 +0530

    5)Adding Attendance Update method
    I added a new method to the Student class. This lets us increase the attendance percentage by 5 percent each time a student attends a class. Now the data can actually change.

---
+++ src/com/attendance/core/Updating Student.java
@@ -0,0 +1,5 @@
+// This method adds 5% to the attendance
+    public void addAttendance() {
+        this.attendance = this.attendance + 5.0;
+        System.out.println("Marking attendance for: " + getName());
+    }
