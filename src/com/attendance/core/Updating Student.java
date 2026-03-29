5773d7f398b9e7b47c463fa578e3e28589beb369
Aheli052 <ahelibhaumikkgp@gmail.com>
2026-03-29 10:31:22 +0530

    Delete src/com/attendance/core/Updating Student.java

--- src/com/attendance/core/Updating Student.java
+++
@@ -1,5 +0,0 @@
-// This method adds 5% to the attendance
-    public void addAttendance() {
-        this.attendance = this.attendance + 5.0;
-        System.out.println("Marking attendance for: " + getName());
-    }
